7a728b2eee1aca5a708ce8cf23b1c9e1cd37a2b3
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-09-30 13:35:37 -0700

    created & initialized constructor with dummy data

--- src/app/itv-time.ts
+++ src/app/itv-time.ts
@@ -1,2 +1,9 @@
 export interface ITvTime {
+  id: number,
+  name: string,
+  status: string,
+  lang: string,
+  networkName: string,
+  image: string,
+  previousEpisode: string,
 }
--- src/app/tv-time/tv-time.component.ts
+++ src/app/tv-time/tv-time.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITvTime } from '../itv-time';
 
 @Component({
   selector: 'app-tv-time',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tv-time.component.css']
 })
 export class TvTimeComponent implements OnInit {
-
-  constructor() { }
+  show: ITvTime
+  constructor() {  
+    this.show = {
+      id: 139,
+      name: "Girls",
+      status: "Ended",
+      lang: "English",
+      networkName: "HBO",
+      image: "https://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg",
+      previousEpisode: "",
+    }//id, name, status,lang, networkName, image, previousEpisode
+  }
 
   ngOnInit(): void {
   }
