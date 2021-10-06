9e282c2433f49c29ef4fd37ec353f9c64c9fda05
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-10-06 10:19:29 -0700

    fixed issues with the merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TvTimeService } from './tv-time.service';
 import { TvTimeComponent } from './tv-time/tv-time.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -16,7 +14,6 @@ import { TvSearchComponent } from './tv-search/tv-search.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
->>>>>>> 4f8f6a44d7dacf129dc39d9bd97ed53112281ea4
 
 @NgModule({
   declarations: [
@@ -27,9 +24,7 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    HttpClientModule
-=======
+    HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatFormFieldModule,
@@ -37,7 +32,6 @@ import {MatCardModule} from '@angular/material/card';
     MatInputModule,
     MatButtonModule,
     MatCardModule
->>>>>>> 4f8f6a44d7dacf129dc39d9bd97ed53112281ea4
   ],
   providers: [TvTimeService],
   bootstrap: [AppComponent]
