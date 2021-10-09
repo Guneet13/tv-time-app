03b8506a282011b2b66bb0cefb609e54c7b251bf
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-10-08 22:33:07 -0700

    fixed multistring and unnecessary trigger issue

--- src/app/tv-search/tv-search.component.ts
+++ src/app/tv-search/tv-search.component.ts
@@ -10,16 +10,17 @@ import { debounceTime } from 'rxjs/operators';
 export class TvSearchComponent implements OnInit {
   @Output() searchEvent = new EventEmitter<string>();
   search = new FormControl('', [Validators.minLength(1)])
+  /* By default it will show null in the text value & adding a validation of minimum length of 1 characters for a tv show name*/
   constructor() { }
 
+  /* emit is taking whatever is coming from text box and passing it to @Output. Emit can go crazy with every time a value changes, it will trigger. Debounce time actually helps throttle(means control) with a lapse of 1 sec to let user finish typing and then trigger*/
   ngOnInit(): void {
     this.search.valueChanges
     .pipe(debounceTime(1000))
     .subscribe((searchValue: string) => {
-      if(!this.search.invalid){
-        this.searchEvent.emit(searchValue);
+      if(!this.search.invalid ){
+        this.searchEvent.emit(searchValue)
       }
     })
   }
-
 }
--- src/app/tv-time.service.ts
+++ src/app/tv-time.service.ts
@@ -15,8 +15,6 @@ export class TvTimeService {
   /*make a call to api url with http request. Gets the observable of IdataFilter, unwrap it(using library called map) and pass it to transform function using pipe function(pipe the data to map to unwrap it from observable around it and then send it over to transform function)*/
   getShowData(name:string){
     return this.httpClient.get<IDataFilter>(`https://api.tvmaze.com/singlesearch/shows?q=${name}`).pipe(map(data=> this.transformToITvTime(data)));
-
-   
   } 
   private transformToITvTime(data: IDataFilter): ITvTime{
     return{
