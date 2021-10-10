9c051e46cc3390829495d24b0c19e8ec6f115c5b
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-10-10 14:06:10 -0700

    handled null on image property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ITvTime } from './itv-time';
 import { TvTimeService } from './tv-time.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -23,15 +22,13 @@ export class AppComponent {
     image: "",
     summary: "",
   }
-
   constructor(private tvTimeService: TvTimeService) { }
-
-
 
   doSearch(searchValue: string){
       const userInput = searchValue.replace(/\s+/g,"%20");
       if(userInput){
-        this.tvTimeService.getShowData(userInput).subscribe(data=> this.showData = data);
+        this.tvTimeService.getShowData(userInput)
+        .subscribe(data=> this.showData = data);
       }
   }
 }
--- src/app/tv-time.service.ts
+++ src/app/tv-time.service.ts
@@ -4,7 +4,6 @@ import { IDataFilter } from './idata-filter';
 import { ITvTime } from './itv-time';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +13,8 @@ export class TvTimeService {
 
   /*make a call to api url with http request. Gets the observable of IdataFilter, unwrap it(using library called map) and pass it to transform function using pipe function(pipe the data to map to unwrap it from observable around it and then send it over to transform function)*/
   getShowData(name:string){
-    return this.httpClient.get<IDataFilter>(`https://api.tvmaze.com/singlesearch/shows?q=${name}`).pipe(map(data=> this.transformToITvTime(data)));
+      return this.httpClient.get<IDataFilter>(`https://api.tvmaze.com/singlesearch/shows?q=${name}`).pipe(map(data=> this.transformToITvTime(data)
+    ));
   }
   private transformToITvTime(data: IDataFilter): ITvTime{
     return{
@@ -26,7 +26,7 @@ export class TvTimeService {
       rating: data.rating.average,
       genres: data.genres.join(", "),
       network: data.network?  data.network.name : data.webChannel?.name,
-      image: data.image.original,
+      image: data.image?.original,
       summary: data.summary.replace( /(<([^>]+)>)/ig, '') //The original summary from TVmaze API has HTML tags, Which should be hidden from users. This replace() removes HTML tags from summary.
     }
   }
